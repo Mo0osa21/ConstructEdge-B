ec5365aadcce156b692c1374f7343a39801efc20
Moosa <moosajamel.21@gmail.com>
2024-12-25 16:40:11 +0300

    Index Model

--- models/index.js
+++ models/index.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose')
 const userSchema = require('./User')
-const postSchema = require('./Post')
+const cartSchema = require('./Cart')
+const categorySchema = require('./Category')
+const orderSchema = require('./Order')
+const productSchema = require('./Product')
 
 const User = mongoose.model('User', userSchema)
-const Post = mongoose.model('Post', postSchema)
+const Cart = mongoose.model('Cart', cartSchema)
+const Category = mongoose.model('Category', categorySchema)
+const Order = mongoose.model('Order', orderSchema)
+const Product = mongoose.model('Product', productSchema)
 
 module.exports = {
   User,
-  Post
+  Cart,
+  Category,
+  Order,
+  Product
 }
