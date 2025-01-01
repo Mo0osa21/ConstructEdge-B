48c8354e1e0e0f3554d4bb4a18fcf27ffae2d520
Moosa <moosajamel.21@gmail.com>
2025-01-01 18:07:00 +0300

    password

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
-    passwordDigest: { type: String, required: true },
+    passwordDigest: { type: String, required: true, min: 8 },
     isAdmin: { type: Boolean, default: false }
   },
   { timestamps: true }
